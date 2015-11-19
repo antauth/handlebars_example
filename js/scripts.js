87c9a38020d6ee7d182bb2bb451a3c5f94bb840c
Antoinette Smith <antoinette@primeacademy.io>
2015-11-18 20:41:12 -0600

    Can add new employees to list via form

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ function Employee(num, firstName, lastName, salary, reviewScore) {
  * Employee list.
  */
 function EmployeeList() {
- 	var list = [];
+ 	list = [];
  	/**
  	 * Get a copy of the list
  	 */
@@ -62,21 +62,41 @@ function EmployeeList() {
  	var list = new EmployeeList();
 
  	// add some employees to the list
- 	list.addEmployee(new Employee(1, "Alicia", "Keys", 50000, 4));
- 	list.addEmployee(new Employee(3, "Michael", "Shanks", 40000, 3));
- 	list.addEmployee(new Employee(5, "Larry", "Whitmore", 70000, 2));
- 	list.addEmployee(new Employee(9, "Maggie", "Gyllenhaal", 72000, 5));
+ 	list.addEmployee(new Employee(1, 'Alicia', 'Keys', 50000, 4));
+ 	list.addEmployee(new Employee(3, 'Michael', 'Shanks', 40000, 3));
+ 	list.addEmployee(new Employee(5, 'Larry', 'Whitmore', 70000, 2));
+ 	list.addEmployee(new Employee(9, 'Maggie', 'Gyllenhaal', 72000, 5));
 
  	// create template for displaying employees
- 	var employeeTemplate = $("#employee-list").html();
+ 	var employeeTemplate = $('#employee-list').html();
 
  	// Compile the template
-  	var theTemplate = Handlebars.compile(employeeTemplate);
+  	var template = Handlebars.compile(employeeTemplate);
 
   	// Pass our data to the template
-  	var compiledHtml = theTemplate({employees: list.getList()});
+  	var compiledHtml = template({employees: list.getList()});
 
   	// Add the compiled html to the page
-  	$(".employees").append(compiledHtml);
+  	$('.employees').append(compiledHtml);
+
+  	/** ------------ FORM SUBMISSION --------------- **/
+  	$('form').on('submit', function(e){
+  		e.preventDefault();
+
+  		$num = $('#num').val();
+  		$firstName = $('#firstName').val();
+  		$lastName = $('#lastName').val();
+  		$reviewScore = $('input[name=reviewScore]:checked').val();
+  		$salary = $('#salary').val();
+
+  		list.addEmployee(new Employee($num, $firstName, $lastName, $salary, $reviewScore));
+
+  		// clear existing data
+  		$('.employees').empty();
+  		// Pass our data to the template
+  		compiledHtml = template({employees: list.getList()});
+  		// Add the compiled html to the page
+  		$('.employees').append(compiledHtml);
+  	});
 
  });
